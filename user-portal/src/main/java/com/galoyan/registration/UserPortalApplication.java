5515ae19e74941c9b316a06cd557785eabdf547b
BELLA GALOYAN <bellayin@gmail.com>
2018-04-19 12:19:13 +0400

    angular 5 and spring boot sipmle app to register users

---
+++ user-portal/src/main/java/com/galoyan/registration/UserPortalApplication.java
@@ -0,0 +1,12 @@
+package com.galoyan.registration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserPortalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserPortalApplication.class, args);
+	}
+}
